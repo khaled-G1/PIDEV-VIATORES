6365512432689ee5966809943b3273abc658590e
khaled-G1 <61561456+khaled-G1@users.noreply.github.com>
2022-03-08 23:54:40 +0100

    Commit_Final

---
+++ Projet/src/Controller/ClientController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditProfileType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("/client", name="client")
+     */
+    public function index()
+    {
+        return $this->render('client/profile.html.twig');
+    }
+
+    /**
+     * @Route("/client/profil/modifier", name="client_profil_modifier")
+     */
+    public function editProfile(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('client');
+        }
+
+        return $this->render('client/editprofile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
